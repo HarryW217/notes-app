626e3b54aae97ec2f30fa7bddfd44b7ca13a1a90
HarryW217 <harrywalker217@gmail.com>
2024-02-01 14:26:06 +0000

    Build basic home endpoint, and import the views and auth blueprints in __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -6,4 +6,7 @@ def create_app():
     config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, 'config.py')
     app.config.from_pyfile(config_path)
     
+    from .views import views
+    from .auth import auth
+    
     return app
